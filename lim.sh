700fcf19c4c9f266fa44b8615644ee88af4d5d76
Sami Tabet <salph.tabet@gmail.com>
2018-03-13 22:51:36 +0100

    generate a uuid for the new cgroup

--- lim.sh
+++ lim.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-cgcreate -g cpu,memory:/lim
-trap "cgdelete cpu,memory:lim" INT TERM EXIT
+rnd_str() {
+  cat /dev/urandom | tr -dc 'a-z0-9' | fold -w $1 | head -n 1
+}
+
+generate_uuid () {
+  echo "$(rnd_str '8')-$(rnd_str '4')-$(rnd_str '4')-$(rnd_str '4')-$(rnd_str '12')"
+}
+
+cgroup_name="lim-$(generate_uuid)"
+
+trap "cgdelete cpu,memory:$cgroup_name" INT TERM EXIT
+
+cgcreate -g cpu,memory:/$cgroup_name
 while getopts ":m:c:" option
 do
-	case $option in
-		m)
-			cgset -r memory.limit_in_bytes=$OPTARG memorylim
-			;;
-		c)
-		vim 	cgset -r cpu.shares=$OPTARG cpulim
-			;;
-		:)
-			echo "The $OPTARG options requires an argument" >&2
-			exit 1
-			;;
-		\?)
-			echo "$OPTARG : invalid option"
-			exit 1
-			;;
-	esac
+  case $option in
+  m)
+    cgset -r memory.limit_in_bytes=$OPTARG $cgroup_name
+    ;;
+  c)
+    cgset -r cpu.shares=$OPTARG $cgroup_name
+    ;;
+  :)
+    echo "The $OPTARG options requires an argument" >&2
+    exit 1
+    ;;
+  \?)
+    echo "$OPTARG : invalid option"
+    exit 1
+    ;;
+  esac
 done
 
 shift $((OPTIND-1))
-sudo cgexec -g cpui,memory:/cpulim eval $*
+cgexec -g cpu,memory:$cgroup_name $*
 exit 0
