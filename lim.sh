3ee08e2a60666a4884d810d562789afa2dc6ce41
Sami Tabet <salph.tabet@gmail.com>
2018-03-13 22:14:52 +0100

    initial commit

---
+++ lim.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cgcreate -g cpu,memory:/lim
+while getopts ":m:c:" option
+do
+	case $option in
+		m)
+			cgset -r memory.limit_in_bytes=$OPTARG memorylim
+			;;
+		c)
+		vim 	cgset -r cpu.shares=$OPTARG cpulim
+			;;
+		:)
+			echo "The $OPTARG options requires an argument" >&2
+			exit 1
+			;;
+		\?)
+			echo "$OPTARG : invalid option"
+			exit 1
+			;;
+	esac
+done
+
+shift $((OPTIND-1))
+sudo cgexec -g cpui,memory:/cpulim eval $*
+exit 0
